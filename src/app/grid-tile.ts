37be0075a06e88c6937b46c5155b0ee74fd78697
WilliamLQin <william.qin51@gmail.com>
2018-01-04 01:21:37 -0500

    fixed flipped size 2.5 hex, added more icons, allow more main text customization

--- src/app/grid-tile.ts
+++ src/app/grid-tile.ts
@@ -16,6 +16,7 @@ export class GridTile {
 const hexSizeOneSVGPoints = '128 0 17.15 64 17.15 192 128 256 238.85 192 238.85 64 128 0';
 const hexSizeTwoSVGPoints = '445.65 256 334.85 192 334.85 64 224 0 113.15 64 113.15 192 2.3 256 2.3 384 113.15 448 224 384 334.85 448 445.7 384 445.65 256';
 const hexSizeTwopFiveSVGPoints = '443.35 309.13 554.25 245.13 554.25 117.13 443.4 53.13 332.55 117.13 221.7 53.13 110.85 117.13 110.85 245.13 0 309.13 0 437.13 110.85 501.13 221.7 437.13 332.55 501.13 443.4 437.13 443.35 309.13';
+const hexSizeTwopFiveFlippedSVGPoints = '110.9 309.13 0 245.13 0 117.13 110.85 53.13 221.7 117.13 332.55 53.13 443.4 117.13 443.4 245.13 554.25 309.13 554.25 437.13 443.4 501.13 332.55 437.13 221.7 501.13 110.85 437.13 110.9 309.13';
 const hexSizeThreeSVGPoints = '554.2 204.53 554.2 76.5 443.35 12.5 332.5 76.5 221.65 12.5 110.8 76.5 110.8 204.53 -0.05 268.55 -0.05 396.55 110.8 460.51 110.8 588.5 221.65 652.5 332.5 588.5 443.35 652.5 554.2 588.5 554.2 460.52 665.05 396.55 665.05 268.55 554.2 204.53';
 const hexSizeFiveSVGPoints = '997.68 682.25 1108.5 618.25 1108.5 490.25 997.68 426.25 997.65 298.25 886.85 234.25 886.8 106.25 775.95 42.25 665.1 106.25 554.25 42.25 443.4 106.25 332.55 42.25 221.7 106.25 221.7 234.25 110.85 298.25 110.85 426.25 -0.01 490.25 -0.01 618.25 110.85 682.25 110.85 810.25 221.7 874.25 221.7 1002.25 332.55 1066.25 443.4 1002.25 554.25 1066.25 665.1 1002.25 775.95 1066.25 886.8 1002.25 886.83 874.25 997.65 810.25 997.68 682.25'
 
@@ -72,7 +73,7 @@ export const TILES: GridTile[] = [
         imagePath: "../assets/hex/Icon Hexes/Hex_Devpost.svg",
         mainPath: "../assets/hex/Main Images/Main_Devpost-min.jpg",
         title: "Devpost",
-        description: "View my hackathon projects on Devpost!",
+        description: "View my hackathon projects\non Devpost!",
         tabTitle: "",
         targetURL: "devpost",
         hovering: false
@@ -86,7 +87,7 @@ export const TILES: GridTile[] = [
         imagePath: "../assets/hex/Icon Hexes/Hex_Resume.svg",
         mainPath: "../assets/hex/Main Images/Main_Resume-min.jpg",
         title: "Resume",
-        description: "View my resume.",
+        description: "<br>View my resume.",
         tabTitle: "",
         targetURL: "resume",
         hovering: false
@@ -153,7 +154,7 @@ export const TILES: GridTile[] = [
         column: 5.25,
         row: -2.5,
         size: 2.5,
-        svgPoints: hexSizeTwopFiveSVGPoints,
+        svgPoints: hexSizeTwopFiveFlippedSVGPoints,
         imagePath: "../assets/hex/Icon Hexes/Hex_Fihgt.svg",
         mainPath: "../assets/hex/Main Images/Main_Fihgt-min.jpg",
         title: "Fihgt",
@@ -192,6 +193,20 @@ export const TILES: GridTile[] = [
     },
     {
         index: 12,
+        column: -5.5,
+        row: -4.5,
+        size: 1.75,
+        svgPoints: hexSizeTwoSVGPoints,
+        imagePath: "../assets/hex/Icon Hexes/Hex_Talk2Control.svg",
+        mainPath: "../assets/hex/Main Images/Main_Talk2Control-min.jpg",
+        title: "Talk2Control",
+        description: "A web application that controls your screen using voice recognition!",
+        tabTitle: " | Talk2Control",
+        targetURL: "talk2control",
+        hovering: false
+    },
+    {
+        index: 13,
         column: -3.25,
         row: 1.5,
         size: 2.5,
@@ -202,6 +217,20 @@ export const TILES: GridTile[] = [
         description: "I program robots and create apps for my FRC team!",
         tabTitle: " | Robotics",
         targetURL: "robotics",
+        hovering: false
+    },
+    {
+        index: 14,
+        column: -3.5,
+        row: 3,
+        size: 1,
+        svgPoints: hexSizeOneSVGPoints,
+        imagePath: "../assets/hex/Icon Hexes/Hex_Lightboard.svg",
+        mainPath: "../assets/hex/Main Images/Main_Lightboard-min.jpg",
+        title: "Lightboard\nCreator",
+        description: "<br>One of the major projects I worked on at D&R Electronics!",
+        tabTitle: " | Lightboard Creator",
+        targetURL: "lightboardcreator",
         hovering: false
     }
     
